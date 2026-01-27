75bac2624b5a6a40f8a2e7e580a7d12bbb21eea2
Avigail Yazdi <ayazdi@g.jct.ac.il>
2026-01-27 14:11:51 +0200

    adding number of products in cart

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -1,10 +1,27 @@
+import { useContext } from "react";
 import { SortSection } from "./SortSection";
+import { ShopContext } from "../ShopContext";
+import { DrawerSection } from "./DrawerSection";
+import { IconButton } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import Badge from '@mui/material/Badge';
 
 export const NavSection = () => {
+  const { cart, toggleDrawer } = useContext(ShopContext);
+  const totalItems = Object.values(cart).reduce((sum, amount) => sum + amount, 0);
+
   return (
     <nav className="product-filter">
       <h1>Products</h1>
       <SortSection />
+      <div className="cartBtnDiv">
+        <IconButton className="cartButton" onClick={toggleDrawer(true)} color="primary">
+          <Badge badgeContent={totalItems} color="error">
+            <ShoppingCart fontSize="large" />
+          </Badge>
+        </IconButton>
+      </div>
+      <DrawerSection />
     </nav>
   );
 };
--- src/components/SortSection.jsx
+++ src/components/SortSection.jsx
@@ -2,13 +2,9 @@ import { useContext } from "react";
 import { ShopContext } from "../ShopContext";
 import { FilterSortComp } from "./FilterSortComp";
 import RangeSlider from "./RangeSlider";
-import { DrawerSection } from "./DrawerSection";
-import { IconButton } from "@mui/material";
-import { ShoppingCart } from "@mui/icons-material";
-
 
 export const SortSection = () => {
-  const { cart, categories, handleCatChange, handleSortChange, selectedCategory, selectedSort, toggleDrawer } = useContext(ShopContext);
+  const { cart, categories, handleCatChange, handleSortChange, selectedCategory, selectedSort } = useContext(ShopContext);
 
   const sortOptions = [
     "No sort",
@@ -18,20 +14,11 @@ export const SortSection = () => {
     "Price, high to low"
   ];
 
-  const totalItems = Object.values(cart).reduce((sum, amount) => sum + amount, 0);
-
   return (
     <div className="sort">
       <FilterSortComp label={"Filter by"} listOfOptions={categories} handleChange={handleCatChange} value={selectedCategory} />
       <FilterSortComp label={"Sort by"} listOfOptions={sortOptions} handleChange={handleSortChange} value={selectedSort} />
       <RangeSlider></RangeSlider>
-      <div className="cartBtnDiv">
-        <IconButton className={"cartButton"} onClick={toggleDrawer(true)} color="primary">
-          <ShoppingCart />
-        </IconButton>
-        <span>{totalItems}</span>
-      </div>
-      <DrawerSection></DrawerSection>
     </div >
   );
 };
