d5ec3e1b9d788e5df91b5a6043e9959d7c58a3c7
Avigail Yazdi <ayazdi@g.jct.ac.il>
2026-02-03 23:01:31 +0200

    update fetch after render

--- src/api/products-functions.js
+++ src/api/products-functions.js
@@ -1,23 +1,23 @@
 export const handleProducts = async () => {
-    const response = await fetch("http://localhost:3000/api/products");
+    const response = await fetch("https://server-gr38.onrender.com/api/products");
     if (!response.ok) throw new Error("Failed to fetch products");
     return response.json();
 };
 
 export const fetchSingleProduct = async (productId) => {
-    const response = await fetch(`http://localhost:3000/api/products/${productId}`);
+    const response = await fetch(`https://server-gr38.onrender.com/api/products/${productId}`);
     if (!response.ok) throw new Error("Failed to fetch product");
     return response.json();
 };
 
 export const deleteProduct = async (productId) => {
-    const response = await fetch(`http://localhost:3000/api/products/${productId}`, { method: "DELETE" });
+    const response = await fetch(`https://server-gr38.onrender.com/api/products/${productId}`, { method: "DELETE" });
     if (!response.ok) throw new Error("Failed to delete product");
     return response.json();
 };
 
 export const addProduct = async (newProduct) => {
-    const response = await fetch(`http://localhost:3000/api/products`, {
+    const response = await fetch(`https://server-gr38.onrender.com/api/products`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newProduct)
@@ -27,7 +27,7 @@ export const addProduct = async (newProduct) => {
 }
 
 export const updateProduct = async (updatedProduct) => {
-    const response = await fetch(`http://localhost:3000/api/products/${updatedProduct._id}`, {
+    const response = await fetch(`https://server-gr38.onrender.com/api/products/${updatedProduct._id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(updatedProduct)
